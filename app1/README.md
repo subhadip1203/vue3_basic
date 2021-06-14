f5a45c2e6c48ab89bcec6f9baabdf8fb79d3505d
subhadip1203 <subhadip1203@gmail.com>
2021-06-15 00:50:59 +0530

    first commit

---
+++ app1/README.md
@@ -0,0 +1,13 @@
+###  audit prod dependency
+command : `npm audit --production`
+
+### run dev server
+command : `npm run serve`
+
+## Vue basic
+    1. data
+
+### data 
+data : is a function return object. <br>
+1. those object properties can be used inside template
+2. try to not use "v-html" : because it can push dangerous  code inside html (XSS issue)
---
+++ app1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>just some text : {{ title.toUpperCase() }}</p>
+  {{HtmlText}}
+  <div v-html="HtmlText"></div>
+  <p v-if="isActive"> Active</p>
+  <p v-else> Inactive</p>
+
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => {
+    return {
+      title: "some random text",
+      HtmlText: "<h1>Trying to mimic P tag </h1>",
+      isActive: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
