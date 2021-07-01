3f6411b37340a185e22f8ab007e517fa435d8d9b
subhadip1203 <subhadip1203@gmail.com>
2021-07-01 22:32:17 +0530

    app4 : vuex part 1

---
+++ app4/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>counter : {{$store.state.count}}</h1>
+        <button> Substract</button>
+        <input class="my_input" type="text" v-model="my_num" />
+        <button> Add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data: function() {
+        return{            
+            my_num:1
+        }
+       
+    }
+}
+</script>
+
+<style scoped>
+    .my_input{
+        margin:10px;
+    }
+</style>
